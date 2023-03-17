bf47aec75950454d337f7a9a704c3f975337091c
David Hidalgo Garzon <david.hidalgo065@gmail.com>
2023-03-16 20:20:23 -0500

    Initial commit

---
+++ test/cdk-primer-reto.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkPrimerReto from '../lib/cdk-primer-reto-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-primer-reto-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkPrimerReto.CdkPrimerRetoStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
